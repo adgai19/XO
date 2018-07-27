be6e54dfe717889e18587bd822d606f7b18e32b2
Aditya Gaitonde <gaitondeaditya@gmail.com>
2018-07-27 20:42:17 +0530

    made some chenges in algorithm and fixed a bug

--- xov2.c
+++ xov2.c
@@ -11,6 +11,8 @@ int main(){
         for(moves=0;moves<9;moves++){
                 printf("enter position to put x or o(row,Columns):\n" );
                 scanf("%d,%d",&i,&j );
+                i--;
+                j--;
                 if(pos[i*3+j]!=0 || i>2 || j>2){
                         printf("Invalid Input");
                         moves--;
@@ -22,6 +24,15 @@ int main(){
                         xo[i][j]='x';
 
                 else xo[i][j]='o';
+
+                for(i=0;i<3;i++){
+                        for(j=0;j<3;j++){
+                                //if(xo[i][j]!='y')
+                                printf("%c ",xo[i][j] );
+                                }
+                                printf("\n" );
+                        }
+
                 for(i=0;i<3;i++){
                         if(xo[i][0]=='x' && xo[i][1]=='x' && xo[i][2]=='x'){
                                 win='x';
@@ -33,12 +44,12 @@ int main(){
                 //                goto winner;
                                 break;
                         }
-                        if(xo[0][i]=='x' && xo[0][i]=='x' && xo[0][i]=='x'){
+                        if(xo[0][i]=='x' && xo[1][i]=='x' && xo[2][i]=='x'){
                                 win='x';
                 //                goto winner;
                                 break;
                         }
-                        if(xo[0][i]=='o' && xo[0][i]=='o' && xo[0][i]=='o'){
+                        if(xo[0][i]=='o' && xo[1][i]=='o' && xo[2][i]=='o'){
                                 win='o';
                 //                goto winner;
                                 break;
@@ -46,15 +57,16 @@ int main(){
 
 
                 }
-/*                if(win=='x' || win=='o')
+                if(win=='x' || win=='o')
                 break;
-*/
+
 
                 if(xo[0][0]=='x' && xo[1][1]=='x' && xo[2][2]=='x'){
                         win='x';
                         break;
                 }
                 if(xo[0][0]=='o' && xo[1][1]=='o' && xo[2][2]=='o'){
+                        break;
                         win='o';
                         break;
                 }
@@ -66,13 +78,7 @@ int main(){
                         win='o';
                         break;
                 }
-                for(i=0;i<3;i++){
-                        for(j=0;j<3;j++){
-                                //if(xo[i][j]!='y')
-                                printf("%c ",xo[i][j] );
-                                }
-                                printf("\n" );
-                        }
+
                 }if(win!='t')
                 /*winner:*/printf("%c wins\n",win );
 
