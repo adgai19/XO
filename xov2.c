5389caee7044ee9f298e5d634a034b7d3cfa439e
adgai19 <gaitondeaditya@gmail.com>
2018-08-01 18:54:21 +0530

    Add files via upload

--- xov2.c
+++ xov2.c
@@ -8,10 +8,11 @@ int main(){
                 for(j=0;j<3;j++)
                 xo[i][j]=' ';
         }
-        for(moves=0;moves<9;moves++)
-        {
+        for(moves=0;moves<9;moves++){
                 printf("enter position to put x or o(row,Columns):\n" );
                 scanf("%d,%d",&i,&j );
+                i--;
+                j--;
                 if(pos[i*3+j]!=0 || i>2 || j>2){
                         printf("Invalid Input");
                         moves--;
@@ -23,62 +24,60 @@ int main(){
                         xo[i][j]='x';
 
                 else xo[i][j]='o';
-                for(i=0;i<3;i++)
-                {
+                for(i=0;i<3;i++){
+                        for(j=0;j<3;j++){
+                                //if(xo[i][j]!='y')
+                                printf("%c ",xo[i][j] );
+                                }
+                                printf("\n" );
+                        }
+                for(i=0;i<3;i++){
                         if(xo[i][0]=='x' && xo[i][1]=='x' && xo[i][2]=='x'){
                                 win='x';
-                                moves=9;
+                //                goto winner;
                                 break;
                         }
                         if(xo[i][0]=='o' && xo[i][1]=='o' && xo[i][2]=='o'){
                                 win='o';
-                                moves=9;
+                //                goto winner;
                                 break;
                         }
                         if(xo[0][i]=='x' && xo[1][i]=='x' && xo[2][i]=='x'){
                                 win='x';
-                                moves=9;
+                //                goto winner;
                                 break;
                         }
                         if(xo[0][i]=='o' && xo[1][i]=='o' && xo[2][i]=='o'){
                                 win='o';
-                                moves=9;
+                //                goto winner;
                                 break;
                         }
+
+
                 }
-                if(xo[0][0]=='x' && xo[1][1]=='x' && xo[2][2]=='x')
-                {
-                          win='x';
-                          break;
+                if(win=='x' || win=='o')
+                break;
+
+
+                if(xo[0][0]=='x' && xo[1][1]=='x' && xo[2][2]=='x'){
+                        win='x';
+                        break;
                 }
-                if(xo[0][0]=='o' && xo[1][1]=='o' && xo[2][2]=='o')
-                    {
-                            win='o';
-                            break;
-                    }
-                if(xo[0][2]=='x' && xo[1][1]=='x' && xo[2][0]=='x')
-                    {
-                            win='x';
-                            break;
-                    }
-                if(xo[0][2]=='o' && xo[1][1]=='o' && xo[2][0]=='o')
-                    {
-                            win='o';
-                            break;
-                    }
-                for(i=0;i<3;i++)
-                {
-                            for(j=0;j<3;j++){
-                                    //if(xo[i][j]!='y')
-                                    printf("%c ",xo[i][j] );
-                                    }
-                                    printf("\n" );
-                            }
+                if(xo[0][0]=='o' && xo[1][1]=='o' && xo[2][2]=='o'){
+                        win='o';
+                        break;
                 }
-                if(win!='t')
-                printf("%c wins\n",win );
-                else
-                  printf("The match is a tie!!");
+                if(xo[0][2]=='x' && xo[1][1]=='x' && xo[2][0]=='x'){
+                        win='x';
+                        break;
+                }
+                if(xo[0][2]=='o' && xo[1][1]=='o' && xo[2][0]=='o'){
+                        win='o';
+                        break;
+                }
+
+                }if(win!='t')
+                /*winner:*/printf("%c wins\n",win );
 
 
 }
